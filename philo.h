611f9e0bd7b5e7b9df1549d17a63f350cf9cd25e
Emmatosorus <emma@bouteyres.com>
2024-04-30 17:53:21 +0200

    Add✨ : Started creating functions and added structure

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: epolitze <epolitze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 11:38:14 by epolitze          #+#    #+#             */
-/*   Updated: 2024/04/30 15:16:49 by epolitze         ###   ########.fr       */
+/*   Updated: 2024/04/30 17:50:41 by epolitze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,17 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 int	ft_atoi(const char *str);
+
+typedef struct s_philo
+{
+	int		id;
+	bool	fork;
+	int		sleep_time;
+	int		eat_time;
+}			t_philo;
 
 #endif //PHILO_GIT_PHILO_H
