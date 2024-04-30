03dcc48600b32bff00d8d21c6459480ca836404a
Emmatosorus <emma@bouteyres.com>
2024-04-30 17:21:57 +0200

    Added some stuff with threads

--- main.c
+++ main.c
@@ -5,15 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: epolitze <epolitze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/29 11:38:03 by epolitze          #+#    #+#             */
-/*   Updated: 2024/04/29 11:48:32 by epolitze         ###   ########.fr       */
+/*   Created: 2024/04/30 09:52:11 by epolitze          #+#    #+#             */
+/*   Updated: 2024/04/30 17:04:53 by epolitze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	*routine()
+{
+	printf("Lemme think!\n");
+	sleep(2);
+	printf("Ending life\n");
+	return (NULL);
+}
+
+//void	create_philosopher()
+//{
+//
+//}
+
 int	main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	pthread_t	t1;
+	pthread_t	t2;
+	int			nb_philo;
+
+	if (ac < 5 || ac > 6)
+		return (1);
+	nb_philo = ft_atoi(av[1]);
+	if (nb_philo > 201 || nb_philo < 1)
+		return (1);
+	pthread_create(&t1, NULL, &routine, NULL);
+	pthread_create(&t2, NULL, &routine, NULL);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: epolitze <epolitze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 11:38:14 by epolitze          #+#    #+#             */
-/*   Updated: 2024/04/29 11:46:50 by epolitze         ###   ########.fr       */
+/*   Updated: 2024/04/30 15:16:49 by epolitze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+int	ft_atoi(const char *str);
 
 #endif //PHILO_GIT_PHILO_H
